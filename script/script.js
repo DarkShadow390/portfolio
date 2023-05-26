555db50c494760ea4cc6e57b7c3bec3c67f8b419
DarkShadow390 <angeokou39@gmail.com>
2023-05-26 11:01:48 +0200

    codage du script en cours

---
+++ script/script.js
@@ -0,0 +1,50 @@
+let code = document.getElementById("motDePasse");
+let codeButton = document.querySelectorAll("button");
+
+codeButton[0].addEventListener("click", function () {
+    // ajoute le chiffre 1 dans le code
+    alert(1);
+});
+codeButton[1].addEventListener("click", function () {
+    // ajoute le chiffre 2 dans le code 
+    alert("2");
+});
+codeButton[2].addEventListener("click", function () {
+    // ajoute le chiffre 3 dans le code 
+    alert("3");
+});
+codeButton[3].addEventListener("click", function () {
+    // ajoute le chiffre 4 dans le code 
+    alert("4");
+});
+codeButton[4].addEventListener("click", function () {
+    // ajoute le chiffre 5 dans le code 
+    alert("5");
+});
+codeButton[5].addEventListener("click", function () {
+    // ajoute le chiffre 6 dans le code 
+    alert("6");
+});
+codeButton[6].addEventListener("click", function () {
+    // ajoute le chiffre 7 dans le code 
+    alert("7");
+});
+codeButton[7].addEventListener("click", function () {
+    // ajoute le chiffre 8 dans le code 
+    alert("8");
+});
+codeButton[8].addEventListener("click", function () {
+    // ajoute le chiffre 9 dans le code 
+    alert("9");
+});
+codeButton[9].addEventListener("click", function () {
+    // ajoute le chiffre 0 dans le code 
+    alert("0");
+});
+
+
+
+if (code == "1234") {
+    //ajoute une animation au "circle7"
+};
+
